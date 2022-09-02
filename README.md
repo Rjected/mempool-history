4601c06e1e4500233c08178210c465fbd45f606f
Dan Cline <6798349+Rjected@users.noreply.github.com>
2022-09-02 14:35:29 -0400

    feat: use new transactions_unordered stream design
     * since it doesn't make much sense to care about hash receipt time,
       just wrap transactions_unordered
     * specifically, wrap a `Stream<Item = Result<Transaction, T>>` where
       `ProviderError` is `From<T>`. The error type should be made more
       generic, because this essentially assumes the `Stream` is a
       `TransactionStream`, when it could in theory be something else with a
       different error type.

--- README.md
+++ README.md
@@ -4,13 +4,6 @@
 [![Test and Lint](https://github.com/Rjected/mempool-history/actions/workflows/ci.yml/badge.svg)](https://github.com/Rjected/mempool-history/actions/workflows/ci.yml)
 
 This tool uses ethereum pending transaction RPCs to listen for mempool transactions and timestamp them.
-It will also log **when** we are notified about a transaction (or transaction hash), allowing us to order mempool transactions by time of receipt.
-
-This does NOT:
- * Track when the _provider or node_ received the transaction over p2p.
- * Track when the transaction was _sent_ over p2p.
- * Accurately capture p2p transaction exchange timing.
-   * Specifically, this tool timestamps when transaction _hashes_ are received via websocket subscription.
 
 The purpose of this tool is to gather data that roughly matches the "true" mempool history so it can be easily replayed while testing other applications.
 
@@ -27,10 +20,6 @@ cargo run -- --rpc-url <YOUR_WEBSOCKET_URL>
 ## TODO
  - [ ] Persist timestamped transactions
    - [ ] Figure out how to gracefully recover after a crash. Make sure the tool does not overwrite timestamps of a transaction that already exists. (_stretch goal_)
- - [ ] sorted stream? (_stretch goal_)
-   - [ ] Prioritize ordering `Transaction`s that are not yet included in a block
- - [ ] Look into performance benefits from multiple websocket connections or multiple transaction senders
-   - [ ] To do this, there needs to be some performance _metric_. What is a good or important metric?
  - [ ] Simplify if on alchemy by using `alchemy_pendingTransactions`
  - [ ] Prettier or more useful output (JSON? frontend?) (_stretch goal_)
    - [ ] Prioritize showing `Transaction`s that are not yet included in a block
--- src/timed_tx.rs
+++ src/timed_tx.rs
@@ -1,13 +1,14 @@
-use std::time::{Duration, SystemTime};
-
-use ethers::types::{Transaction, TxHash};
+use ethers::{providers::ProviderError, types::Transaction};
+use futures_core::stream::Stream;
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+    time::{Duration, Instant, SystemTime},
+};
 
 /// This represents a transaction, its hash, and a timestamp
 #[derive(Debug, Clone)]
 pub struct TimedTransaction {
-    /// The transaction hash we used to request the transaction from the provider
-    pub hash: TxHash,
-
     /// The time the transaction hash was received
     pub timestamp: SystemTime,
 
@@ -16,28 +17,61 @@ pub struct TimedTransaction {
 }
 
 impl TimedTransaction {
-    /// Constructs a [`TimedTransaction`] from a [`TimedHash`], computing the timestamp by adding
-    /// the timed hash `duration` to the [`SystemTime`] provided.
-    ///
-    /// This populates the [`TimedTransaction`] transaction with the `full_tx` provided.
+    /// Constructs a [`TimedTransaction`] from a [`Transaction`], computing the timestamp by adding
+    /// the `Duration` to the [`SystemTime`] provided.
     #[inline(always)]
-    pub fn from_timed_hash(timed: TimedHash, since: SystemTime, full_tx: Transaction) -> Self {
+    pub fn timestamp_tx(full_tx: Transaction, since: SystemTime, elapsed: Duration) -> Self {
         TimedTransaction {
-            hash: timed.hash,
             // could panic, let's keep the return type Self for now, we can use checked_add if we
             // really need to.
-            timestamp: since + timed.duration,
+            timestamp: since + elapsed,
             transaction: full_tx,
         }
     }
 }
 
-#[derive(Debug, Clone)]
-/// This represents a hash with the duration elapsed since the program started
-pub struct TimedHash {
-    /// A transaction hash
-    pub hash: TxHash,
+/// This represents a stream similar to [`TransactionStream`](ethers_providers::TransactionStream),
+/// but with timestamped transactions.
+pub struct TimedTransactionStream<St> {
+    /// The transaction stream being timestamped
+    pub stream: St,
 
-    /// The duration since the program started
-    pub duration: Duration,
+    /// The time when the stream started to be timestamped
+    pub start: SystemTime,
+
+    /// The instant when the stream started to be timestamped
+    pub instant: Instant,
+}
+
+impl<St> TimedTransactionStream<St> {
+    /// Create a new `TransactionStream` instance, setting the internal `time` and `instant` to
+    /// `now`.
+    pub fn new(stream: St) -> Self {
+        Self {
+            stream,
+            start: SystemTime::now(),
+            instant: Instant::now(),
+        }
+    }
+}
+
+impl<'a, St, T> Stream for TimedTransactionStream<St>
+where
+    St: Stream<Item = Result<Transaction, T>> + Unpin + 'a,
+    ProviderError: From<T>,
+{
+    type Item = Result<TimedTransaction, ProviderError>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let this = self.get_mut();
+        match Stream::poll_next(Pin::new(&mut this.stream), cx) {
+            Poll::Ready(tx) => Poll::Ready(tx.map(|tx| {
+                tx.map(|non_stamped| {
+                    TimedTransaction::timestamp_tx(non_stamped, this.start, this.instant.elapsed())
+                })
+                .map_err(|err| ProviderError::from(err))
+            })),
+            _ => Poll::Pending,
+        }
+    }
 }
